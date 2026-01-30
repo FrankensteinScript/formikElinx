71a28eaf45cee8f26cf0ce3949d4cc75a8e404e9
Patrik <patrikludvik@gmail.com>
2026-01-30 05:46:03 +0100

    test run app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const theme = createTheme();
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</StrictMode>,
+);
